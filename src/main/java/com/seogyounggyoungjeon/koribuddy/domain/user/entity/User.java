f7226ee00cd0141b3689a1b6a1162a59ae337254
Altair5869 <th0538@naver.com>
2025-07-16 23:23:32 +0900

    Refactor: Member -> User 변경

--- src/main/java/com/seogyounggyoungjeon/koribuddy/domain/user/entity/User.java
+++ src/main/java/com/seogyounggyoungjeon/koribuddy/domain/user/entity/User.java
@@ -1,4 +1,4 @@
-package com.seogyounggyoungjeon.koribuddy.domain.member.entity;
+package com.seogyounggyoungjeon.koribuddy.domain.user.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
@@ -9,7 +9,8 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @ToString
-public class Member {
+@Table(name = "member")
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
