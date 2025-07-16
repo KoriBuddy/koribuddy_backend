b43dc859aaaa294542c1e59c835f6b09709732f7
Altair5869 <th0538@naver.com>
2025-07-16 22:02:41 +0900

    Feat: First Setting

---
+++ src/main/java/com/seogyounggyoungjeon/koribuddy/KoribuddyApplication.java
@@ -0,0 +1,13 @@
+package com.seogyounggyoungjeon.koribuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoribuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoribuddyApplication.class, args);
+    }
+
+}
